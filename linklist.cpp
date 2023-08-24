81a40b3629c61ac25dd27dd0ea7e6ed4d3a634a5
kingpin-ay <ayushmondal001@gmail.com>
2023-08-24 19:26:30 +0530

    added Makefile

--- linklist.cpp
+++ linklist.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include<iostream>
 #include "linklist.h"
 #include "node.h"
@@ -17,12 +18,87 @@ void LinkList::addElementToLast(int value){
     this -> totalElements++;
 }
 
+
+void LinkList::addElementToFirst(int value){
+    Node* newNode = new Node(value);
+    newNode -> setNextNodeAddress(this->head);
+    this->head = newNode;
+    this->totalElements++;
+}
+
+void LinkList::addElementToIndex(int index , int value){
+    if(index <= 0) this ->addElementToFirst(value);
+    else if(index >= this->totalElements - 1) this -> addElementToLast(value);
+    else{
+        Node* iterator = this->head;
+        for(int i = 0; i < index ; i++){
+            iterator = iterator -> getNextNodeAddress();
+        }
+        Node* newNode = new Node(value);
+        newNode -> setNextNodeAddress(iterator->getNextNodeAddress());
+        iterator -> setNextNodeAddress(newNode);
+        this -> totalElements++;
+    }
+}
+
+
 void LinkList::travaseLinkList() {
     Node* iterator = this -> head;
     for(int i = 0 ; i<this-> totalElements ; i++){
         std::cout << iterator ->getStoredData() << "\t";
         iterator = iterator -> getNextNodeAddress();
-    }    
+    }   
+    std::cout << "\n";
 }
+
+
+void LinkList::deleteElementFromLast(){
+
+    Node* iterator = this -> head;
+    for(int i = 0 ; i< this-> totalElements - 2 ; i++){
+        iterator = iterator -> getNextNodeAddress();
+    }
+    iterator -> setNextNodeAddress(NULL);
+    this -> tail = iterator;
+    this -> totalElements--;
+}
+
+
+void LinkList::deleteElementFromFirst(){
+    Node* tempPointerToHead = this -> head;
+    head = head -> getNextNodeAddress();
+    tempPointerToHead -> setNextNodeAddress(NULL);
+    tempPointerToHead -> setStoredData(0);
+    this -> totalElements--;
+}
+
+
+void LinkList::deleteElementFromIndex(int index){ 
+    if(this -> totalElements <= 0) std::cout << "No Node exists"<<"\n"; 
+    else{
+        if(index >= this -> totalElements -1) this -> deleteElementFromLast();
+        else if (index <= 0 ) this -> deleteElementFromFirst();
+        else {
+            Node* iterator = this -> head;
+            for(int i = 0 ; i< index-1 ; i++){
+                iterator = iterator -> getNextNodeAddress();
+            }
+            Node* nextNodePoint = iterator -> getNextNodeAddress() -> getNextNodeAddress();
+            iterator -> setNextNodeAddress(nextNodePoint);
+            this -> totalElements--;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
--- linklist.h
+++ linklist.h
@@ -10,7 +10,10 @@ class LinkList {
         LinkList(int value);
         void addElementToLast(int value);
         void travaseLinkList();
+        void addElementToFirst(int value);
+        void addElementToIndex(int index , int value);
         void deleteElementFromLast();
-
+        void deleteElementFromFirst();
+        void deleteElementFromIndex(int index);
 };
 #endif
