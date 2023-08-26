7accbfd59614aa46a457627dcf8f08dec3e7e852
kingpin-ay <ayushmondal001@gmail.com>
2023-08-26 22:12:51 +0530

    updated the delete functions

--- cLinkList/clinklist.h
+++ cLinkList/clinklist.h
@@ -6,9 +6,18 @@ class CLinkList {
     public:
         Node* head;
         Node* tail;
-    
+        int totalElements;
+
         CLinkList(int value);
         void traverse();
+    
+        void addNodeToLast(int value);
+        void addNodeToFirst(int value);
+        int addNodeToIndex(int index , int value);
+        
+        void deleteNodeFromLast();
+        void deleteNodeFromFirst();
+        void deleteNodeFromIndex(int index);
 };
 
 #endif
